15eb847fb84c49fd2a467d0d3ddd2f601bbf2016
Prajeesh MP <meetprajeesh@gmail.com>
2024-01-08 16:29:10 +0530

    Update: Demo URL

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -3,18 +3,24 @@ interface Props {
     description: string;
     imgUrl: string;
     downloadUrl: string;
+    demoUrl?: string;
 }
 
-function Template({ title, description, imgUrl, downloadUrl }: Props): JSX.Element {
+function Template({ title, description, imgUrl, downloadUrl, demoUrl }: Props): JSX.Element {
     return (
         <section className="mb-4">
             <h2>{title}</h2>
             <p className="lead">{description}</p>
             <img src={imgUrl} alt={title} className="img-fluid" />
             <p>
-                <a href={downloadUrl} target="_blank" className="btn btn-success mt-3" rel="noreferrer">
+                <a href={downloadUrl} target="_blank" className="btn btn-success mt-3 me-2" rel="noreferrer">
                     Download
                 </a>
+                {demoUrl && (
+                    <a href={demoUrl} target="_blank" className="btn btn-primary mt-3" rel="noreferrer">
+                        Demo
+                    </a>
+                )}
             </p>
         </section>
     );
